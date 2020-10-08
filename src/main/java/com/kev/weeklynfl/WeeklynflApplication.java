e3444e1d1f2fb1a140392fbb3ba0a22494a71358
Kevin Staniszewski <kevingstanisz@gmail.com>
2020-10-07 20:15:53 -0500

    first commit

---
+++ src/main/java/com/kev/weeklynfl/WeeklynflApplication.java
@@ -0,0 +1,13 @@
+package com.kev.weeklynfl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklynflApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklynflApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kev/weeklynfl/WeeklynflApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kev.weeklynfl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklynflApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
